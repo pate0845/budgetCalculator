b412aa8dd1b1d9bf7786a7147714f263a5d64f23
pate0845 <pate0845@algonquinlive.com>
2022-01-19 15:01:23 -0500

    Displaying all the expense and description.

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 let budgetDisplay=document.getElementById('budget-display-amount');
 let expenseDisplay=document.getElementById('budget-display-expense');
 let savingsDisplay=document.getElementById('budget-display-saving');
-let data=[];
+
+let inputAmount=document.getElementById('budget-amount');
+let inputDescription=document.getElementById('budget-description');
+let inputExpense=document.getElementById('budget-expense');
+let buttonSubmit=document.getElementById('budget-data-submit');
+
+let displayDescription=document.getElementById('description');
+let displayExpense=document.getElementById('expense');
+
+var data=[];
+var id=0;
+
+
+buttonSubmit.onclick=function(){
+    addExpenses(inputDescription.value,inputExpense.value,id)
+    id++;
+};
+
 
 function addExpenses(description,amount,id){
-
+    let exp=new ExpenseFactor(description,amount,id);
+    data[id]=exp;
+    
+    function ExpenseFactor(description,amount,id){
+        this.description=description;
+        this.amount=amount;
+        this.id=id;
+    }
+    displayDescription.innerHTML+=`
+                                   <span  class="" value=${id}>
+                                   <span class="desc" >${data[id].description}</span>
+                                   <span class="amt" >${data[id].amount}</span>
+                                   </span>
+                                   <br>`;
 }
-
-function ExpenseFactor(description,amount,id){
-    this.id=id;
-    this.description=description;
-    this.amount=amount;
-}
-
-
-display(40,budgetDisplay);
-display(23,expenseDisplay);
-display(25,savingsDisplay);
-
 
 function display(setAmount,element){
     element.innerText=setAmount;
 }
 
+function displayList(arr){
+    for(let i=0;i<arr.length;i++){
+    }
+}
 
 
