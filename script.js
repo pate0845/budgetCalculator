8cfb9c2f2c8e2dc2128285a752e27dfbb55f3488
pate0845 <pate0845@algonquinlive.com>
2022-02-15 12:06:36 -0500

    comments added.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
 let view={
+    //update the display with id
     setDisplay:function(elementId,value){
         let text=document.getElementById(elementId);
         text.innerHTML=value;
     },
+    //helps to retrive the value 
     getValue:function(elementId){
         let text=document.getElementById(elementId);
         return text;
     },
+    //displays error message
     displayMessage:function(message){
         this.setDisplay('message',message);
     },
+    //display all the expense
     displayItems:function(data,divElement){
         divElement.innerHTML='';
         data.forEach((i,n)=> {
@@ -29,17 +33,22 @@ let view={
 
 
 let model={
+    //avoid mutation added array into object
     data:[],
+    //total budget
     budget:0,
+    //constructor to create object
     CreateObj:function(item,amount){
         this.item=item;
         this.amount=amount;
     },
+    //method to add objects to array 
     addObject:function(item,amount){
         let obj=new this.CreateObj(item,amount);
         this.data.push(obj);
         view.displayItems(this.data,document.getElementById('display-data'));
     },
+    //calculate total whenever array is changed
     calculateTotal:function(arr){
         let newArr=arr;
         let total=0;
@@ -48,9 +57,11 @@ let model={
         }
         return total;
     },
+    //total savings by subtracting expense from income
     calculateSavings:function(expense){
         return this.budget-expense;
     },
+    //deletes element from an array
     deleteExpense:function(id){
         this.data.splice(id,1);
         let deleteElement=view.getValue(`${id}`);
@@ -64,6 +75,7 @@ let model={
 
 
 let controller={
+    //method adds the expense to the list view
     addExpenseToSpan:function(){
         let item=view.getValue('budget-description');
         let amount=view.getValue('budget-expense');
@@ -79,9 +91,11 @@ let controller={
             return view.displayMessage("You need to enter all the information!");
         }
     },
+    //method updates the budget
     updateBudget:function(value){
         view.setDisplay('budget-display-amount',value);
     },
+    //method to update the expense
     updateExpense:function(){
         let expense=model.calculateTotal(data);
         view.setDisplay('budget-display-expense',expense);
